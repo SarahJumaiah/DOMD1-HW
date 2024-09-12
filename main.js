909947abd5c596ace429a006a88e34015b4a00cb
sarahjumaiah <sarahjumaiah@gmail.com>
2024-09-12 08:43:28 +0300

    cv done

---
+++ main.js
@@ -0,0 +1,118 @@
+function pic() {
+    document.getElementById("img").src = "https://media.licdn.com/dms/image/v2/D4E03AQEdxtVv5hWRgg/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1723992216887?e=1731542400&v=beta&t=W59TwaA_TXSKprz8GCSAcW3WG8wxgQniYb449oJr3lA";
+}
+//
+
+function phone() {
+    document.getElementById("phone").textContent = "+966506661875";
+}
+
+function email() {
+    document.getElementById("email").textContent = "sarahjumaiah@gmail.com";
+}
+
+function address() {
+    document.getElementById("address").textContent = "AlKharj, Riyadh";
+}
+
+function website() {
+    document.getElementById("website").textContent = "https://www.linkedin.com/in/sarah-aljumaiah-a6989b234/";
+}
+
+//
+function master() {
+    document.getElementById("master").style.display = "none"
+}
+
+function bachelor() {
+    document.getElementById("bacheloryear").textContent = "2020 - 2024"; 
+    document.getElementById("bacheloruni").textContent = "Prince Sattam University";
+    document.getElementById("bachelordegree").textContent = "• Bachelor of Computer Science";
+    document.getElementById("bGPA").style.display = "none";
+
+}
+
+//
+function skill1() {
+    document.getElementById("skill1").textContent = "HTML";
+}
+
+function skill2() {
+    document.getElementById("skill2").textContent = "CSS";
+}
+
+function skill3() {
+    document.getElementById("skill3").textContent = "JavaScript";
+}
+
+function skill4() {
+    document.getElementById("skill4").textContent = "bootstrap";
+}
+
+function skill5() {
+    document.getElementById("skill5").textContent = "DOM";
+}
+
+function skill6() {
+    document.getElementById("skill6").textContent = "Java";
+}
+
+function skill7() {
+    document.getElementById("skill7").textContent = "SQL";
+}
+
+//
+function language() {
+    document.getElementById("english").textContent = "English (Intermediate)";
+    document.getElementById("french").textContent = "Arabic (Native)";
+    document.getElementById("german").style.display = "none"
+    document.getElementById("spanish").style.display = "none"
+}
+//
+function myname() {
+    document.getElementById("name").textContent = "Sarah";
+    document.getElementById("lastname").textContent = "Jumaiah";
+    document.getElementById("position").textContent = "Full Stack Developer";
+}
+//
+function profile() {
+    document.getElementById("protit").textContent = "Objective";
+    document.getElementById("prodes").textContent = "A fresh Computer Science graduate with a strong passion for technology seeks a position to gain hands-on experience, expand skills in IT support or software development, and learn industry best practices. Committed to building a solid foundation for a successful career in the tech industry through meaningful project contributions and continuous learning of new programming languages and technologies ";
+}
+//
+function exp() {
+    document.getElementById("exp1").textContent = "CyberSecurity club PSAU (2022 - 2024)";
+    document.getElementById("role1").textContent = "Data Entry and Support ";
+    document.getElementById("task1-1").textContent = "Handling attendance tracking, student data, and registration/survey forms to support the cybersecurity club and helps maintain records.";
+    document.getElementById("task1-2").style.display = "none"
+    document.getElementById("task1-3").style.display = "none"
+}
+
+function exp2() {
+    document.getElementById("where1").textContent = "Education Department in Al-Kharj Governorate (2024 - 2024)";
+    document.getElementById("role2").textContent = "Software Development and IT Support";
+    document.getElementById("task2-1").textContent = "The training covered everything from troubleshooting to UI/UX design, app development, and database building, providing a comprehensive experience. ";
+    document.getElementById("task2-2").textContent = "Collaborate with cross-functional teams...";
+    document.getElementById("task2-3").textContent = "Maintain code quality and technical documentation...";
+}
+
+function exp3() {
+    document.getElementById("exp3").style.display = "none"
+}
+
+//
+function ref() {
+    document.getElementById("reftit").textContent = "Projects";
+    
+    document.getElementById("ref1Name").innerHTML = "<strong>Prediction of Depression for X users </strong><br> ";
+    document.getElementById("ref1Contact").innerHTML = "web tool that analyzes X profiles to predict depression, Leveraging a Kaggle dataset and a support vector machine (SVM) algorithm, the system uses natural language processing to identify patterns correlated with signs of depression. Users can enter their X username to receive a personalized depression risk percentage and tailored recommendations, empowering them to proactively monitor their mental well-being.";
+    
+    document.getElementById("ref2Name").style.display = "none"
+    document.getElementById("ref2Contact").style.display = "none"
+}
+
+
+
+
+
+
